fe98454303798a2104e5611a46a92ee62ac9f1ed
Hoang Hiep <HoangHiep0901@gmail.com>
2016-10-03 23:04:09 +0700

    test ignore

--- springexamples/src/main/java/edu/java/spring/App.java
+++ springexamples/src/main/java/edu/java/spring/App.java
@@ -4,10 +4,8 @@ package edu.java.spring;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
